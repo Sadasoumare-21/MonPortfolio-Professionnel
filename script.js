9df0e4f07d7f4bcb0eea0b036614a901d3290bef
Sadasoumare-21 <sadasoumare018@gmail.com>
2026-02-13 02:46:17 +0000

    ajout d'alerte

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ function Envoyer() {
     const nom = document.getElementById("nom");
     const email = document.getElementById("email");
     const message = document.getElementById("message");
-    const messageAlert = document.getElementById("alert");
+    const affic = document.getElementById("affic");
 
     if (nom.value == "" || email.value == "" || message.value == "") {
-        messageAlert.textContent = "Veuillez remplir tous les champs.";
-        messageAlert.className = "text-red-500 mt-3 text-center font-semibold";
+        affic.textContent = "Veuillez remplir tous les champs.";
         return;
     } else {
-        messageAlert.textContent = "Merci pour votre message, je vous répondrai dans les plus brefs délais !";
-        messageAlert.className = "text-green-500 mt-3 text-center font-semibold";
+        affic.textContent = "Merci pour votre message, je vous répondrai dans les plus brefs délais !";
     }
+
 }
